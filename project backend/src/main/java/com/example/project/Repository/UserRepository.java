40b29c8d12029c8978b383cb519f2b9b34c332c6
Shifa pansare <shifapansare22@gmail.com>
2024-02-19 11:32:28 +0530

    updated

---
+++ project backend/src/main/java/com/example/project/Repository/UserRepository.java
@@ -0,0 +1,22 @@
+package com.example.project.Repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.example.project.Entities.User;
+
+import jakarta.transaction.Transactional;
+
+@Transactional
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+	@Modifying
+	@Query("delete from User u where userid=:id")
+	public int delete(int id);
+	
+}
